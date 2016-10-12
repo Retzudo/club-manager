0503146201c20c5f8e4a306fbc0ab5545d6125fa
Retzudo <christoph.willinger@gmail.com>
2016-10-12 15:52:21 +0200

    Models

---
+++ clubbot/core/models.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.db import models
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through='Membership',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+    club = models.ForeignKey(
+        'Club',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    club = models.ForeignKey(
+        'Club',
+        on_delete=models.CASCADE,
+    )
+    role = models.ForeignKey(
+        'Role',
+        null=True,
+        on_delete=models.SET_NULL
+    )
