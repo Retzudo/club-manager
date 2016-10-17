62dcbb7f0dc3afdec599603253f22c9287dad9c3
Retzudo <christoph.willinger@gmail.com>
2016-10-17 22:04:50 +0200

    CSS

--- clubbot/clubbot/settings.py
+++ clubbot/clubbot/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'emailuser',
+    'widget_tweaks',
     'rest_framework',
     'rest_framework.authtoken',
     'core',
