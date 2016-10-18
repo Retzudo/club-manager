77502890a4c6ad5b5ed73956a671cefa2e1090c4
Retzudo <christoph.willinger@gmail.com>
2016-10-18 23:04:44 +0200

    Transactions API

--- clubbot/api/rest.py
+++ clubbot/api/rest.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api import serializers
 from core.models import Membership, Club, Role
@@ -20,3 +24,28 @@ class ClubViewSet(viewsets.ModelViewSet):
         )
 
         default_membership.save()
+
+
+class CashView(APIView):
+    def get(self, request, club_id, *args, **kwargs):
+        club = get_object_or_404(Club, pk=club_id)
+        serializer = serializers.CashSerializer(club.cash)
+
+        return Response(serializer.data)
+
+
+class TransactionsList(APIView):
+    def get(self, request, club_id, *args, **kwargs):
+        club = get_object_or_404(Club, pk=club_id)
+        serializer = serializers.TransactionSerializer(club.cash.transactions.all(), many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, club_id, *args, **kwargs):
+        club = get_object_or_404(Club, pk=club_id)
+        serializer = serializers.TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(cash=club.cash)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- clubbot/api/serializers.py
+++ clubbot/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Club
+from core.models import Club, Cash, Transaction
 
 
 class ClubSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,3 +10,15 @@ class ClubSerializer(serializers.HyperlinkedModelSerializer):
         # Slug is not required because it will be generated
         # if left out.
         extra_kwargs = {'slug': {'required': False}}
+
+
+class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('amount', 'description', 'date')
+
+
+class CashSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Cash
+        fields = ('currency', 'total')
