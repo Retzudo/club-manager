d0f0bc1f95a0f6f27ad2ec9f7bff7396cc9cbb3e
Retzudo <christoph.willinger@gmail.com>
2017-04-13 13:17:30 +0200

    Upgraded Django and installed REST framework JWT Auth

--- clubmanager/clubmanager/settings.py
+++ clubmanager/clubmanager/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'emailuser',
-    'widget_tweaks',
     'rest_framework',
     'rest_framework.authtoken',
     'core',
