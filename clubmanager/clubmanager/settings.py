a236f3ca48c992e3920e779dc90afc904f70dc3a
Retzudo <christoph.willinger@gmail.com>
2017-04-13 13:26:39 +0200

    Added JWT auth

--- clubmanager/clubmanager/settings.py
+++ clubmanager/clubmanager/settings.py
@@ -130,6 +130,7 @@ LOGIN_URL = '/login'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
