31f704ca61032fbc0276d24ba286b18e02a67e9e
Retzudo <christoph.willinger@gmail.com>
2017-04-19 15:24:38 +0200

    Test button

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Button} from 'semantic-ui-react';
 
 export default class App extends React.Component {
     render() {
         return (
             <div style={{textAlign: 'center'}}>
                 <h1>Weyo</h1>
+                <Button>ASDF</Button>
             </div>
         )
     }
